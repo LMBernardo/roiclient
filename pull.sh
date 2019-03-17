4460095c2927254385b4804eade0054b51d13205
Layne Bernardo <lmbernar@uark.edu>
2019-03-16 20:55:19 -0500

    Simplified pull.sh

--- pull.sh
+++ pull.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 git submodule init
 git submodule update
-git submodule foreach --recursive git submodule init
-git submodule foreach --recursive git submodule update
-git submodule foreach --recursive git submodule init
-git submodule foreach --recursive git submodule update
